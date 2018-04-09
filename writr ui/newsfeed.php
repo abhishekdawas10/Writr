68e5ea16d01694f7034ef8f50644ecd1fb0ff5b9
abhishekgoyal1 <abhi.goyal2@gmail.com>
2018-04-09 15:01:23 +0530

    UI update 1

---
+++ writr ui/newsfeed.php
@@ -0,0 +1,30 @@
+<?php
+    $con= mysqli_connect("localhost","root","", "writr");
+    $query= mysqli_query($con, "SELECT * FROM `projects` DESC LIMIT 20");
+    echo "<div class=\"container\">
+    <div class=\"row\">
+        <div class=\"col-lg-8 col-md-10 mx-auto\">";
+    while ($fetch = mysqli_fetch_assoc($query)){
+        $title = nl2br($fetch['name']);
+        $desc = nl2br($fetch['summary']);
+        
+        echo "<div class=\"post-preview\">
+            <a target=\"_blank\" href=\"\">
+              <h2 class=\"post-title\">
+                $title
+              </h2>
+              <h3 class=\"post-subtitle\">
+                $desc....
+              </h3>
+            </a>
+          </div>
+          <hr>";
+    }
+
+    echo "<div class=\"clearfix\">
+            <button class=\"btn btn-primary float-right\" id= \"old\">Older Posts &rarr;</a>
+          </div>
+        </div>
+      </div>
+    </div>";
+?>
